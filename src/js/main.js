be0fc55f6aa1427e8297df62d96b66c3d43007af
Juan Julio Sandoval Layza <juanjus98@gmail.com>
2024-05-07 15:46:50 -0500

    Avance 07-05

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,13 @@ lightbox.init();
 
 /* AOS.js */
 AOS.init();
+
+/* Menú */
+var burgerMenu = document.getElementById("burger-menu");
+
+var overlay = document.getElementById("menu");
+
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
